cf96eb1025ba59c3b74451087100bd8e736c0443
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2022-03-07 11:16:34 +0900

    Reproduce an issue

---
+++ src/without_click.py
@@ -0,0 +1,39 @@
+import logging
+import os
+
+import pymysql
+import sqlalchemy
+from google.cloud.sql.connector import connector
+
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT, level='DEBUG')
+
+
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = connector.connect(
+        os.getenv("CONNECTION_NAME"),
+        "pymysql",
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        db=os.getenv("DATABASE_NAME"),
+    )
+    return conn
+
+
+def main() -> None:
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+    )
+
+    with pool.connect() as conn:
+        result = conn.execute(sqlalchemy.text("select 1")).fetchall()
+
+        for row in result:
+            print(row)
+
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
